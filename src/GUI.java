3893864414e02d9af7cec07a2cbda4d2110083c1
gustavogandrade <andrade.gustavogomes@gmail.com>
2026-05-29 17:06:54 -0300

    added icon

--- src/GUI.java
+++ src/GUI.java
@@ -1,4 +1,5 @@
 import javax.swing.*;
+import java.net.URL;
 
 public class GUI {
 
@@ -21,11 +22,19 @@ public class GUI {
 
         window = new JFrame("Notepad");
         window.setSize(670,670);
+
+        URL iconURL = getClass().getClassLoader().getResource("notepad-icon.png");
+            ImageIcon image = new ImageIcon(iconURL);
+            window.setIconImage(image.getImage());
+            System.out.println("Warning: notepad-icon.png not found in resources!");
+
+
+
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
     public void createTextArea() {
         textArea = new JTextArea();
-        window.add(textArea, scrollPane);
+        window.add(textArea);
         scrollPane = new JScrollPane(textArea, JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED, JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
         window.add(scrollPane);
         scrollPane.setBorder(BorderFactory.createEmptyBorder());
