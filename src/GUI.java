58cfadd6a8f1acec859de15f8cc8a91203ad10c3
gustavogandrade <andrade.gustavogomes@gmail.com>
2026-06-02 13:58:59 -0300

    added menu bar with items and shortcuts defined (yet to be configured).

--- src/GUI.java
+++ src/GUI.java
@@ -10,9 +10,9 @@ public class GUI {
     JTextArea textArea;
     JScrollPane scrollPane;
 
-    JMenu file,newdoc,open,save,print,exit;
-    JMenu edit,copy,paste,cut,selectall;
-    JMenu format,fontfamily,fontstyle,fontsize;
+    JMenu file,newDoc,open,save,print,exit;
+    JMenu edit,copy,paste,cut,selectAll;
+    JMenu format,fontFamily,fontStyle,fontSize;
 
     static void main(String[] args){
         new GUI();
@@ -34,7 +34,8 @@ public class GUI {
         window.setSize(670,670);
 
         URL iconURL = getClass().getClassLoader().getResource("notepad-icon.png");
-            ImageIcon image = new ImageIcon(iconURL);
+        assert iconURL != null;
+        ImageIcon image = new ImageIcon(iconURL);
             window.setIconImage(image.getImage());
             System.out.println("Warning: notepad-icon.png not found in resources!");
 
@@ -58,7 +59,7 @@ public class GUI {
 
         file = new JMenu("File");
 
-        newdoc = new JMenu("New");
+        newDoc = new JMenu("New");
         open = new JMenu("Open");
         save = new JMenu("Save");
         print = new JMenu("Print");
@@ -69,20 +70,20 @@ public class GUI {
         copy = new JMenu("Copy");
         paste = new JMenu("Paste");
         cut = new JMenu("Cut");
-        selectall = new JMenu("Select All");
+        selectAll = new JMenu("Select All");
 
         format = new JMenu("Format");
 
-        fontfamily = new JMenu("Font Family");
-        fontstyle = new JMenu("Font Style");
-        fontsize = new JMenu("Font Size");
+        fontFamily = new JMenu("Font Family");
+        fontStyle = new JMenu("Font Style");
+        fontSize = new JMenu("Font Size");
 
         window.setJMenuBar(menuBar);
         menuBar.add(file);
         menuBar.add(edit);
         menuBar.add(format);
 
-        file.add(newdoc);
+        file.add(newDoc);
         file.add(open);
         file.add(save);
         file.add(print);
@@ -91,24 +92,24 @@ public class GUI {
         edit.add(copy);
         edit.add(paste);
         edit.add(cut);
-        edit.add(selectall);
+        edit.add(selectAll);
 
-        format.add(fontfamily);
-        format.add(fontstyle);
-        format.add(fontsize);
+        format.add(fontFamily);
+        format.add(fontStyle);
+        format.add(fontSize);
     }
 
 
     public void createShortcuts(){
-        newdoc.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_N,ActionEvent.CTRL_MASK));
-        open.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_O,ActionEvent.CTRL_MASK));
-        save.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_S,ActionEvent.CTRL_MASK));
-        exit.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_ESCAPE,ActionEvent.CTRL_MASK));
+        newDoc.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_N, InputEvent.CTRL_DOWN_MASK));
+        open.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_O,InputEvent.CTRL_DOWN_MASK));
+        save.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_S,InputEvent.CTRL_DOWN_MASK));
+        exit.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_ESCAPE,InputEvent.CTRL_DOWN_MASK));
 
-        copy.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_C,ActionEvent.CTRL_MASK));
-        paste.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_V,ActionEvent.CTRL_MASK));
-        cut.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_X,ActionEvent.CTRL_MASK));
-        selectall.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_A,ActionEvent.CTRL_MASK));
+        copy.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_C,InputEvent.CTRL_DOWN_MASK));
+        paste.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_V,InputEvent.CTRL_DOWN_MASK));
+        cut.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_X,InputEvent.CTRL_DOWN_MASK));
+        selectAll.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_A,InputEvent.CTRL_DOWN_MASK));
 
     }
 }
